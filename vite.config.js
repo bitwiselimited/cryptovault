c36646667573dfbc26ea68bfad2f0c4831b0a31e
BitWise Games™ <bitwisevault@gmail.com>
2025-10-20 21:58:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // This forces relative paths
   server: {
-    port: 5173,
-    open: true
-  }
+    port: 3000,
+  },
 })
